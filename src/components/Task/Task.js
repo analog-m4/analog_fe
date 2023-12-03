2f5f68179809c4acba3e5437778319d674be6fa3
Johann Dee <johanndee1994@gmail.com>
2023-12-03 00:39:14 -0800

    Add margin bottom to task

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,7 +1,7 @@
 function Task({ title, description }) {
   return (
     <>
-      <div className="task flex flex-col rounded-md border border-gray-200 p-2 shadow-sm">
+      <div className="task flex flex-col rounded-md border border-gray-200 p-2 shadow-sm mb-3">
         <div className="task-title">{title}</div>
         <div className="task-description font-light text-sm">{description}</div>
       </div>
