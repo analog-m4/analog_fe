b300ab84d3c49e1ff44dbd52778e6df48b493f0a
Logan Matheny <loganpaulmatheny@gmail.com>
2023-12-03 12:35:03 -0500

    Create basic form for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,49 @@
-function Login() {
+import { useState } from "react";
+
+function Login({ handleLogin }) {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const userInfo = {
+      username,
+      password,
+    };
+    handleLogin();
+    clearInputs();
+  }
+
+  function clearInputs() {
+    setUserName("");
+    setPassword("");
+  }
+
   return (
-    <>
-    <h1>Login</h1>
-    </>
+    <form>
+      <input
+        type="text"
+        placeholder="username"
+        name="username"
+        value={username}
+        onChange={(event) => setUserName(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        name="password"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <button
+        className="submit-button"
+        onClick={(e) => handleSubmit(e)}
+        disabled={!username || !password}
+      >
+        Login
+      </button>
+    </form>
   );
 }
 
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,12 +1,14 @@
 import Header from "../Header/Header";
 import About from "../About/About";
+import Login from "../Login/Login";
 
-function Welcome() {
+function Welcome({ handleLogin }) {
   return (
     <>
-      <div className="flex flex-col h-screen">
+      <div className="flex flex-col h-screen items-center">
         <p>Welcome to Analog</p>
         <About />
+        <Login handleLogin={handleLogin} />
       </div>
     </>
   );
