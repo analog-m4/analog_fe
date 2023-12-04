7203dfeff175e78bccc79cf667dcffa3efa1d629
Nicholas McEnroe <nsmcenro@gmail.com>
2023-12-04 15:44:18 -0700

    Edit: Add html, css, and js drafts for file upload

--- src/nick.js
+++ src/nick.js
@@ -1,31 +1,58 @@
 
-// const imageForm = document.querySelector("#imageForm")
-// const imageInput = document.querySelector("#imageInput")
+const fileForm = document.querySelector("#fileForm")
+const fileName = document.querySelector("#fileName ")
+const fileInput = document.querySelector("#fileInput")
 
-// imageForm.addEventListener("submit", async event => {
-//   event.preventDefault()
-//   const file = imageInput.files[0]
+imageForm.addEventListener("submit", async event => {
+  event.preventDefault()
+  const file = fileInput.files[0]
 
-//   // get secure url from our server
-//   const { url } = await fetch("/s3Url").then(res => res.json())
-//   console.log(url)
+fetch("/s3/index")
+.then(response => response.json())
+.then(data => {
+  const { upload_url} = data;
 
-//   // post the image direclty to the s3 bucket
-//   await fetch(url, {
-//     method: "PUT",
-//     headers: {
-//       "Content-Type": "multipart/form-data"
-//     },
-//     body: file
-//   })
+  const file = ''
+  fetch(upload_url, {
+    method: "PUT",
+    body: file,
+    headers: {
+      "Content-Type": file.type
+    }
+  })
+})
+  .then(response => {
+      if (response.ok) {
+        const filename = file.name;
+        const fileLink = upload_url;
 
-//   const imageUrl = url.split('?')[0]
-//   console.log(imageUrl)
+        const data = {
+          name: filename,
+          document: fileLink
+        };
 
-//   // post requst to my server to store any extra data
-  
-  
-//   const img = document.createElement("img")
-//   img.src = imageUrl
-//   document.body.appendChild(img)
-// })
+        fetch('/s3/create', {
+          method: 'POST',
+          headers: {
+            "Content-Type": 'application/json'          
+          },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+          if (response.ok) {
+            console.log("Information successfully stored in database.");
+          } else {
+            console.error("Information not sent successfully");
+          }
+        })
+        .catch(error => {
+          console.error('Error communicating with the back end application');
+        });
+      } else {
+        console.error('Error uploading the file to S3');
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching the upload URL from the backend');
+    });
+})
