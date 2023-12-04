8ab2b7dff546f22c317b06ffbb0eaa60e38476ce
Johann Dee <johanndee1994@gmail.com>
2023-12-03 20:52:40 -0800

    Fix project list

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,12 +3,14 @@ import { NavLink } from "react-router-dom";
 
 function Project({ title, id, onClick }) {
   return (
-    <div className="project flex gap-1 cursor-pointer" onClick={onClick}>
+    <>
       <NavLink to={`/project/${id}`}>
-        <div className="project-color self-center w-2.5 h-2.5 border-purple-600 border bg-purple-600 rounded-full"></div>
-        <div>{title}</div>
+        <div className="project flex gap-1 cursor-pointer" onClick={onClick}>
+          <div className="project-color self-center w-2.5 h-2.5 border-purple-600 border bg-purple-600 rounded-full"></div>
+          <div>{title}</div>
+        </div>
       </NavLink>
-    </div>
+    </>
   );
 }
 
