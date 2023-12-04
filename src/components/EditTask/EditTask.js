9722c3773ea4db22debd9348f00dbad5cb359070
Logan Matheny <loganpaulmatheny@gmail.com>
2023-12-04 09:14:46 -0500

    ID how to combine modale with redux for editting tasks

--- src/components/EditTask/EditTask.js
+++ src/components/EditTask/EditTask.js
@@ -10,6 +10,18 @@ function EditTask() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  function updateTask(id, title, description, status) {
+
+    const updateTasks = user..map((employee) => {
+        if (id == employee.id) {
+            return { ...employee, name: newName, role: newRole };
+        }
+
+        return employee;
+    });
+    setEmployees(updatedEmployees);
+}
+
   return (
     <div>
       EditTask
@@ -19,6 +31,85 @@ function EditTask() {
       >
         Update
       </button>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Update Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form
+            onSubmit={(e) => {
+              handleClose();
+              e.preventDefault();
+              props.updateEmployee(props.id, name, role);
+            }}
+            id="editmodal"
+            className="w-full max-w-sm"
+          >
+            {/* ===== PICKUP HERE ===== */}
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="name"
+                >
+                  Full Name
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="name"
+                  type="text"
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  for="role"
+                >
+                  Role
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="role"
+                  type="text"
+                  value={role}
+                  onChange={(e) => {
+                    setRole(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <button
+            className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
+            onClick={handleClose}
+          >
+            Close
+          </button>
+          <button
+            className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+            form="editmodal"
+          >
+            Update
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
