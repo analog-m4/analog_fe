fd68571f7b120bdee566714e6ee7439356b51ae0
Johann Dee <johanndee1994@gmail.com>
2023-12-05 15:27:49 -0800

    Add stopProagation

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -7,30 +7,20 @@ import { v4 as uuidv4 } from 'uuid';
 function AddTask({ taskStatus }) {
   const dispatch = useDispatch();
 
-  const [show, setShow] = useState(false);
   const [newTaskTitle, setNewTaskTitle] = useState('');
   const [newTaskDescription, setNewTaskDescription] = useState('');
-
+  
+  const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
-  const handleClose = () => {
+  const handleClose = (e) => {
+    e.stopPropagation();
     setShow(false);
     setNewTaskTitle('');
     setNewTaskDescription('');
   }
 
-  const handleAddTask = () => {
-    console.log("Adding task:", newTaskTitle, newTaskDescription, taskStatus)
-    dispatch(addTaskToProject({
-        task_id: uuidv4(), // random uuid
-        title: newTaskTitle,
-        description: newTaskDescription,
-        status: taskStatus, 
-    }));
-    handleClose();
-  }
-
   return (
-    <div className="add-task-btn flex text-gray-400 text-sm items-center cursor-pointer pl-3 pb-3" onClick={handleOpen}>
+    <div className="add-task-btn flex text-gray-400 text-sm items-center cursor-pointer pl-3 pb-3" onClick={handleShow}>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-6 text-green-400 mr-2">
       <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
       </svg>
@@ -45,37 +35,74 @@ function AddTask({ taskStatus }) {
           <Modal.Title>Add Task</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form onSubmit={(e) => {
-            e.preventDefault();
-          }}>
-          <input
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            id="title"
-            type="text"
-            placeholder="Task Title"
-            value={newTaskTitle}
-            onChange={(e) => setNewTaskTitle(e.target.value)}
-          />
-          <input
-            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            id="description"
-            type="text"
-            placeholder="Task Description"
-            value={newTaskDescription}
-            onChange={(e) => setNewTaskDescription(e.target.value)}
-          />
+          <form 
+            onSubmit={(e) => {
+              e.preventDefault();
+              dispatch(
+                addTaskToProject({
+                  task_id: uuidv4(), // random uuid
+                  title: newTaskTitle,
+                  description: newTaskDescription,
+                  status: taskStatus, 
+                })
+              );
+            handleClose();
+          }}
+        >
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                htmlFor="new-task-title"
+              >
+                Title
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="new-task-title"
+                type="text"
+                placeholder="Task Title"
+                value={newTaskTitle}
+                onChange={(e) => setNewTaskTitle(e.target.value)}
+              />
+             </div>
+            </div>
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="new-task-description"
+                >
+                  Description
+                </label>
+              </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="new-task-description"
+              type="text"
+              placeholder="Task Description"
+              value={newTaskDescription}
+              onChange={(e) => setNewTaskDescription(e.target.value)}
+            />
+              </div>
+            </div>
           </form>
         </Modal.Body>
         <Modal.Footer>
-          <button
+        <button
             className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
-            onClick={handleClose}
+            onClick={(e) => {
+              console.log("Close button clicked")
+              handleClose(e)}}
           >
             Close
           </button>
           <button
             className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
-            onClick={handleAddTask}
+            form="add-task"
             >
             Add
           </button>
--- src/components/FileUpload/FileUpload.js
+++ src/components/FileUpload/FileUpload.js
@@ -1,13 +1,13 @@
 function FileUpload() {
   return (
     <div className="flex w-full mb-4">
-      <div class="upload-file">
-        <label for="formFile" class="form-label">Upload File</label>
-        <input class="form-control" type="file" id="formFile" />
+      <div className="upload-file">
+        <label htmlFor="formFile" className="form-label">Upload File</label>
+        <input className="form-control" type="file" id="formFile" />
       </div>
       <div className="upload-file-btn flex items-end pb-2 ml-1 cursor-pointer">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
         </svg>
       </div>
     </div>
