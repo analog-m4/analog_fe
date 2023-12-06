531036999f50d046d84067c71a077d364285f70b
Johann Dee <johanndee1994@gmail.com>
2023-12-05 22:20:18 -0800

    Fix bug for not being able to close modal

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,41 +1,51 @@
 import React, { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
-import { useDispatch, useSelector } from 'react-redux';
-import { addTaskToProject } from '../../reducers/user';
-import { v4 as uuidv4 } from 'uuid';
+import { useDispatch, useSelector } from "react-redux";
+import { addTaskToProject } from "../../reducers/user";
+import { v4 as uuidv4 } from "uuid";
 
 function AddTask({ taskStatus }) {
   const dispatch = useDispatch();
 
-  const [newTaskTitle, setNewTaskTitle] = useState('');
-  const [newTaskDescription, setNewTaskDescription] = useState('');
-  
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+  const [newTaskDescription, setNewTaskDescription] = useState("");
+
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
-  const handleClose = (e) => {
-    e.stopPropagation(); // this makes the close button work but the others not work
+  const handleClose = () => {
     setShow(false);
-    setNewTaskTitle('');
-    setNewTaskDescription('');
-  }
+    // setNewTaskTitle('');
+    // setNewTaskDescription('');
+  };
 
   return (
-    <div className="add-task-btn flex text-gray-400 text-sm items-center cursor-pointer pl-3 pb-3" onClick={handleShow}>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-6 text-green-400 mr-2">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-      </svg>
-     Add Task
-     <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={true}
+    <>
+      <div
+        className="add-task-btn flex text-gray-400 text-sm items-center cursor-pointer pl-3 pb-3"
+        onClick={handleShow}
       >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-5 h-6 text-green-400 mr-2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 4.5v15m7.5-7.5h-15"
+          />
+        </svg>
+        Add Task
+      </div>
+      <Modal show={show} onHide={handleClose} backdrop="static" keyboard={true}>
         <Modal.Header closeButton>
           <Modal.Title>Add Task</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form 
+          <form
             onSubmit={(e) => {
               e.preventDefault();
               dispatch(
@@ -43,31 +53,33 @@ function AddTask({ taskStatus }) {
                   task_id: uuidv4(), // random uuid
                   title: newTaskTitle,
                   description: newTaskDescription,
-                  status: taskStatus, 
+                  status: taskStatus,
                 })
               );
-            handleClose();
-          }}
-        >
-          <div className="md:flex md:items-center mb-6">
-            <div className="md:w-1/3">
-              <label
-                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                htmlFor="new-task-title"
-              >
-                Title
-              </label>
-            </div>
-            <div className="md:w-2/3">
-              <input
-                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                id="new-task-title"
-                type="text"
-                placeholder="Task Title"
-                value={newTaskTitle}
-                onChange={(e) => setNewTaskTitle(e.target.value)}
-              />
-             </div>
+              handleClose();
+            }}
+            id="add-task"
+            className="w-full max-w-sm"
+          >
+            <div className="md:flex md:items-center mb-6">
+              <div className="md:w-1/3">
+                <label
+                  className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                  htmlFor="new-task-title"
+                >
+                  Title
+                </label>
+              </div>
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="new-task-title"
+                  type="text"
+                  placeholder="Task Title"
+                  value={newTaskTitle}
+                  onChange={(e) => setNewTaskTitle(e.target.value)}
+                />
+              </div>
             </div>
             <div className="md:flex md:items-center mb-6">
               <div className="md:w-1/3">
@@ -78,38 +90,36 @@ function AddTask({ taskStatus }) {
                   Description
                 </label>
               </div>
-          <div className="md:w-2/3">
-            <input
-              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-              id="new-task-description"
-              type="text"
-              placeholder="Task Description"
-              value={newTaskDescription}
-              onChange={(e) => setNewTaskDescription(e.target.value)}
-            />
+              <div className="md:w-2/3">
+                <input
+                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                  id="new-task-description"
+                  type="text"
+                  placeholder="Task Description"
+                  value={newTaskDescription}
+                  onChange={(e) => setNewTaskDescription(e.target.value)}
+                />
               </div>
             </div>
           </form>
         </Modal.Body>
         <Modal.Footer>
-        <button
+          <button
             className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
-            onClick={(e) => {
-              console.log("Close button clicked")
-              handleClose(e)}}
+            onClick={handleClose}
           >
             Close
           </button>
           <button
             className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
             form="add-task"
-            >
+          >
             Add
           </button>
         </Modal.Footer>
       </Modal>
-    </div>
-  )
+    </>
+  );
 }
 
 export default AddTask;
