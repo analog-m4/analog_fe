fa8a7c15cd5d2cba9c5a3a60e42a4a766d659a8b
Johann Dee <johanndee1994@gmail.com>
2023-12-07 16:30:04 -0800

    Update login ui

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -20,32 +20,34 @@ function Login({ handleLogin }) {
   }
 
   return (
-    <form className="flex flex-col mt-2">
-      <input
-        className="m-2 w-max"
-        type="text"
-        placeholder="username"
-        name="username"
-        value={username}
-        onChange={(event) => setUserName(event.target.value)}
-      />
-      <input
-        className="m-2 w-max"
-        type="password"
-        placeholder="password"
-        name="password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
+    <div className="login min-w-max p-3 bg-white border border-gray-200 rounded-lg shadow-sm">
+      <form className="flex flex-col mt-2">
+        <input
+          className="m-2 w-max bg-gray-100 p-2 rounded-md italic border border-gray-200"
+          type="text"
+          placeholder="username"
+          name="username"
+          value={username}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <input
+          className="m-2 w-max bg-gray-100 p-2 rounded-md italic border border-gray-200"
+          type="password"
+          placeholder="password"
+          name="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
 
-      <button
-        className="submit-button"
-        onClick={(e) => handleSubmit(e)}
-        disabled={!username || !password}
-      >
-        Login
-      </button>
-    </form>
+        <button
+          className="submit-button text-xs text-white flex items-center font-lato bg-purple-600 h-10 self-center pl-5 pr-5 rounded-3xl mt-2 shadow-sm cursor-pointer hover:bg-purple-500"
+          onClick={(e) => handleSubmit(e)}
+          disabled={!username || !password}
+        >
+          LOGIN
+        </button>
+      </form>
+    </div>
   );
 }
 
