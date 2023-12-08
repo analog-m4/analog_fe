acd8027c9fc144107e32f407a927fdfca3ee4e6c
Johann Dee <johanndee1994@gmail.com>
2023-12-07 19:42:54 -0800

    Update add project styling

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -20,12 +20,12 @@ function Project({ title, id, color }) {
       >
         <div
           className={`project flex gap-1 cursor-pointer hover:font-bold ${
-            selectedProjectId === id ? "text-purple-600 font-bold" : ""
+            selectedProjectId === id ? "text-purple-600 font-bold underline" : ""
           }`}
           onClick={() => dispatch(setSelectedProject(id))}
         >
           <div
-            className="project-color self-center w-2.5 h-2.5 border-600 border bg-600 rounded-full"
+            className="project-color self-center w-3 h-3 border-600 border bg-600 rounded-full"
             style={circleStyle}
           ></div>
           <div>{title}</div>
