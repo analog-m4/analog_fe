744bf792aa3aecf8daf955489ecd6850090f3bac
Johann Dee <johanndee1994@gmail.com>
2023-12-08 00:57:59 -0800

    Add BuiltWith component to About

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,19 @@
+import BuiltWith from "../BuiltWith/BuiltWith";
+
 function About() {
   return (
-    <div className="flex flex-col items-center h-1/2 justify-center gap-5 w-7/12">
+    <div className="flex flex-col items-center pt-4 gap-5 w-7/12">
       <div className="flex flex-col items-center">
+        <div className="font-fjalla text-7xl text-center">The ultimate</div>
         <div className="font-fjalla text-7xl text-center">
-          The ultimate
+          to-do list application.
         </div>
-          <div className="font-fjalla text-7xl text-center">to-do list application.</div>
       </div>
       <div className="flex font-lato text-xl items-center w-1/2 text-center">
         Manage tasks, write notes, and organize projects. Analog is the fastest
         way to get work done!
       </div>
+      <BuiltWith />
     </div>
   );
 }
